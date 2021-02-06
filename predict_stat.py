c3286d9ab2c42adb9beab6a364ac9a1f6b6646e9
PoHung Huang <pohung.pcs08g@nctu.edu.tw>
2021-02-06 17:49:01 +0800

    statistics by group

--- predict_stat.py
+++ predict_stat.py
@@ -7,6 +7,30 @@ import datetime
 from pytz import timezone
 
 
+
+def by_group(df):
+    df['up4_gt'] = (((df['up4'] * 2 - 1) * df['up4_predicted'] + 1) / 2).astype('uint8')
+    df['drop5_gt'] = (((df['drop5'] * 2 - 1) * df['drop5_predicted'] + 1) / 2).astype('uint8')
+    # df.to_excel('all_predicts.xls')
+    by_g = df.groupby(by=['group'])
+    row_list = []
+    for g, g_df in by_g:
+        # print('group:' + g)
+        up4_tp = len(g_df[(g_df.up4 == 1) & (g_df.up4_gt == 1)])
+        up4_recall = up4_tp / g_df['up4_gt'].sum()
+        up4_precision = up4_tp / g_df['up4'].sum()
+        drop5_tp = len(g_df[(g_df.drop5 == 1) & (g_df.drop5_gt == 1)])
+        drop5_recall = drop5_tp / g_df['drop5_gt'].sum()
+        drop5_precision = drop5_tp / g_df['drop5'].sum()
+        row_list.append(
+            {'group': g,
+             'r_up4': up4_recall, 
+             'p_up4': up4_precision,
+             'r_drop5': drop5_recall,
+             'p_drop5': drop5_precision})
+        print('%s r_up4: %.2f, p_up4: %.2f, r_drop5: %.2f, p_drop5: %.2f ' % (g, up4_recall, up4_precision, drop5_recall, drop5_precision))
+    return pd.DataFrame(row_list)
+
 def main():
     pred_folder = sys.argv[1]
     predict_dates = []
@@ -24,7 +48,7 @@ def main():
         date_str = '_'.join(dir.split('_')[1:])
         if date_str not in predict_dates:
             continue
-        print('group: %s, date: %s' % (group_name, date_str))
+        # print('group: %s, date: %s' % (group_name, date_str))
         df = pd.read_csv(data_path, index_col=None, header=0)
         if len(df[df['up4_predicted'] == 0]) == len(df):
             print('Yet ground truth, excluded.')
@@ -33,11 +57,14 @@ def main():
         dfs.append(df)
     stat_df = pd.concat(dfs, axis=0, ignore_index=True)
 
+    today_str = datetime.datetime.now().astimezone(cst).strftime('%Y_%m_%d')
     # statics by date
     # statics by group
+    result_by_group = by_group(stat_df)
+    result_by_group.to_excel(os.path.join(pred_folder, 'by_group_predict_stat_%s.xls' % today_str), index=False)
     # statics by trend
     # statics by raw
-    print(stat_df)
+    #print(stat_df)
     
 
 
